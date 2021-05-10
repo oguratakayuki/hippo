03731b3d8f360666cbf845a2a1c0962c315c268e
takayuki.ogura <takayuki.ogura@stafes.com>
2021-05-11 00:22:22 +0900

    更新した結果を表示できるようにした

--- src/AnimalInfo.tsx
+++ src/AnimalInfo.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import {HippoProps} from './Hippo';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { hippoSlice, hippoState } from './modules/HippoState';
+import { AppState } from './store';
+
 export default function AnimalInfo(props: HippoProps) {
   const { name, age } = props;
+
+  const { hippo } = useSelector<
+    AppState,
+    { hippo: hippoState }
+  >((state) => ({
+    hippo: state.hippo,
+  }));
+  const dispatch = useDispatch();
+
   return (
     <React.Fragment>
       <div className="animal-info">
@@ -11,7 +24,7 @@ export default function AnimalInfo(props: HippoProps) {
           <dt>age</dt><dd>{age}</dd>
           <dt>gender</dt><dd>men</dd>
           <dt>tall</dt><dd>2m</dd>
-          <dt>weight</dt><dd>10kg</dd>
+          <dt>weight</dt><dd>{hippo.weight}kg</dd>
           <dt>status</dt><dd>angry</dd>
         </dl>
       </div>
