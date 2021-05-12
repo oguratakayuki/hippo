b3e3c7568a215e1edc57c6deef4f9f0780c1deba
takayuki.ogura <takayuki.ogura@stafes.com>
2021-05-12 23:22:11 +0900

    動的なselectbox追加.関数名をただす

--- src/Action.tsx
+++ src/Action.tsx
@@ -20,42 +20,57 @@ export default function Action() {
   const { incrementCount, decrementCount } = counterSlice.actions;
   const { addLog, deleteLog, setLogLoading } = logSlice.actions;
   const { feed } = hippoSlice.actions;
-  const hoge = () => {
-    console.log('herererer');
+  const actionTypeChange = (event:  React.ChangeEvent<HTMLSelectElement>) => {
     dispatch(addLog({ id: 1, text: 'hoge' }));
     dispatch(incrementCount());
+    const target = event.target as HTMLElement; 
+    const actionType1: number = Number(event.currentTarget.value);
+    if ([2,3].includes(actionType1)) {
+      const unit = document.querySelector("p#actionValueUnit") as HTMLElement;
+      unit.innerHTML = "時間";
+    } else if ( actionType1 == 1) {
+      const unit = document.querySelector("p#actionValueUnit") as HTMLElement;
+      unit.innerHTML = "個";
+      const secondOption = document.querySelector("#second-option") as HTMLElement;
+      secondOption.style.display = "block";
+    }
   };
-  const fuga = (event: React.FormEvent<HTMLFormElement>) => {
+  const breedAction = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const  target = event.target as HTMLElement; 
+    const target = event.target as HTMLElement; 
     const element: HTMLInputElement = target.querySelector('input[name="actionValue"]') as HTMLInputElement;
     const food: number  = Number(element.value);
     const quantity: number = Number(target.querySelector('select')!.selectedOptions[0].value);
     dispatch(feed({ food: food, quantity: quantity }));
-    console.log('fuga');
   };
 
-
-
-
-    return (
-      <form onSubmit={fuga}>
-        <div className="action-form">
-            <div className="mb-3">
-              <select className="form-select" aria-label="Default select example" name="actionType" onChange={hoge} >
-                <option value="DEFAULT" disabled>Open this select menu</option>
-                <option value="1">feed</option>
-                <option value="2">lesson</option>
-                <option value="3">walk</option>
-              </select>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="option-input" className="form-label">action option</label>
-              <input type="text" className="form-control" id="option-input" name="actionValue"  />
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </div>
-      </form>
-    );
+  return (
+    <form onSubmit={breedAction}>
+      <div className="action-form">
+          <div className="mb-3">
+            <select className="form-select option1" aria-label="Default select example" name="actionType1" onChange={(event) => { actionTypeChange(event) }} >
+              <option value="DEFAULT" disabled>Open this select menu</option>
+              <option value="1">食事</option>
+              <option value="2">訓練</option>
+              <option value="3">お散歩</option>
+            </select>
+          </div>
+          <div className="mb-3" style={{display: 'none'}} id="second-option">
+            <select className="form-select option2" aria-label="Default select example"  id="option-input2" name="actionType2">
+              <option value="DEFAULT" disabled>何を食べさせる?</option>
+              <option value="1">お肉</option>
+              <option value="2">お野菜</option>
+              <option value="3">来場者</option>
+            </select>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="option-input2" className="form-label">action option</label>
+            <input type="number" className="form-control" id="option-input2" name="actionValue" />
+            <p id="actionValueUnit">時間</p>
+          </div>
+          <button type="submit" className="btn btn-primary">実行</button>
+      </div>
+    </form>
+  );
 
 }
